de294bb7d5a018c7f1239cc1a8ef1419fd724d75
Seerat Irshad <seeratirsh@gmail.com>
2026-02-26 22:09:51 +0530

    Add check users route

--- index.js
+++ index.js
@@ -119,7 +119,7 @@ app.get("/setup-db", (req, res) => {
 
 
 app.get("/check-users", (req, res) => {
-  connection.query("SELECT id, name, email, role FROM users", (err, results) => {
+  connection.query("SELECT * FROM users", (err, results) => {
     if (err) {
       console.error(err);
       return res.send("Error fetching users");
