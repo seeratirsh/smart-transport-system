660b29fd842b01aadb502c351dafd3e4a07bf854
Seerat Irshad <seeratirsh@gmail.com>
2026-02-26 19:57:48 +0530

    Configured DB with environment variables for Railway deployment

--- config/db.js
+++ config/db.js
@@ -2,11 +2,14 @@ require("dotenv").config();
 const mysql = require('mysql2');
 
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Seer@t1008',
-    database: 'transport'
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    port: process.env.DB_PORT
 });
+
+module.exports = connection;
 
 connection.connect((err) => {
     if (err) {
